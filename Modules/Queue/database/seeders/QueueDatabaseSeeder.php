9c00ea1a4319f81e2ff83d04a986aa9f9bc6196f
harundemircioglu <harun.demircioglu02@gmail.com>
2025-04-12 21:58:07 +0300

    make a queue module

---
+++ Modules/Queue/database/seeders/QueueDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Queue\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QueueDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
