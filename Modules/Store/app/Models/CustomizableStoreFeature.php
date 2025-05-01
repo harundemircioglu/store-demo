7dfc6781cdbb096256fac57e48530f0a979609dd
harundemircioglu <harun.demircioglu02@gmail.com>
2025-05-01 12:49:16 +0300

    some update

--- Modules/Store/app/Models/CustomizableStoreFeature.php
+++ Modules/Store/app/Models/CustomizableStoreFeature.php
@@ -3,12 +3,15 @@
 namespace Modules\Store\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Log\Traits\Loggable;
 
 class CustomizableStoreFeature extends Model
 {
+    use Loggable;
+
+    protected $guarded = ['id'];
+
     // store_type => 1 = individual, 2 = corporate, 3 = both
 
     // type => 1 = text, 2 = textarea, 3 = select, 4 = checkbox, 5 = radio, 6 = file, 7 = date, 8 = time, 9 = datetime
-
-    protected $guarded = ['id'];
 }
